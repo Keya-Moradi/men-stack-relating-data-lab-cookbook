b748bd5310a5242d792df3d6e5bda1b7e784e239
keya-moradi <keya.moradi@gmail.com>
2024-10-10 23:54:16 -0700

    got it to work

--- controllers/users.js
+++ controllers/users.js
@@ -3,25 +3,28 @@ const router = express.Router();
 const User = require('../models/user');
 
 // Index route - show all users
-router.get('/', (req, res) => {
-  User.find({}, (err, users) => {
-    if (err) {
-      console.log(err);
-      return res.redirect('/');
-    }
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find({}); // Use async/await here
     res.render('users/index.ejs', { users });
-  });
+  } catch (err) {
+    console.log(err);
+    return res.redirect('/'); // Redirect to home on error
+  }
 });
 
 // Show route - show a single user's pantry
-router.get('/:userId', (req, res) => {
-  User.findById(req.params.userId, (err, user) => {
-    if (err || !user) {
-      console.log(err);
-      return res.redirect('/');
+router.get('/:userId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId); // Fetch user using async/await
+    if (!user) {
+      return res.redirect('/'); // If no user found, redirect to home
     }
     res.render('users/show.ejs', { user });
-  });
+  } catch (err) {
+    console.log(err);
+    return res.redirect('/'); // Redirect to home on error
+  }
 });
 
 module.exports = router;
